0ac828968d2e3d28fe87e2a9522128b163ebb7ab
Jonah Sellwood <sellwojona@ecs.vuw.ac.nz>
2022-07-12 17:16:17 +1200

    Initial commit

---
+++ src/net/hackerquacker/ccluatranspiler/Transpiler.java
@@ -0,0 +1,9 @@
+package net.hackerquacker.ccluatranspiler;
+
+public class Transpiler {
+
+    /** Main Entrypoint for the CCLuaTranspiler Program */
+    public static void main(String args[]){
+
+    }
+}
